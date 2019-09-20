0ca600c5ca3c79e7f048afc68b9ebd82f3d2c2eb
Alex Brownlie <alex.brownlie@redembedded.com>
2019-09-20 14:32:43 +0100

    [Dev] Trying to scrape from LSE.

--- web_scraping_LSE.py
+++ web_scraping_LSE.py
@@ -6,37 +6,27 @@ import pandas as pd
 driver = webdriver.Firefox("geckodriver-v0.25.0-linux64")
 
 ## Stores name, price and rating of products
-products=[]
-prices=[]
-ratings=[]
+stocks=[]
+
 ## Sets URL to be scraped
 driver.get("https://www.londonstockexchange.com/exchange/prices-and-markets/stocks/risers-and-fallers/risers-fallers.html")
 
 content = driver.page_source
 soup = BeautifulSoup(content)
-def main():
-    # Finding everything that is in this class
-    for a in soup.findAll('a',href=True, attrs={'class':'column1_nomenu'}):
-        name=a.find('div', attrs={'class':'name'})
-        price=a.find('div', attrs={'class':'price'})
 
-        ## Try except loops so that it can be seen where incorrect classes
-        ## are given - will return "empty" in CSV.
-        try:
-            products.append(name.text)
-        except:
-            products.append("empty")
-            
-        try:
-            prices.append(price.text)
-        except:
-            prices.append("empty")
-            
+my_table=soup.find('table',{'class':'table_dati'})
+#rows = my_table.findAll('a', attrs = {'class' : 'name'})
+for row in my_table.findAll('td', attrs = {'class':'name'}):
+    stock = row.
+    stocks.append(stock)
+    print(stock)
+# Table of Stocks info (Top 10 risers/fallers)
+#print(stocks)
 
-    # Saving data to CSV
-    df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings}) 
-    df.to_csv('stocks.csv', index=False, encoding='utf-8')
+
+# Saving data to CSV
+#df = pd.DataFrame({'Product Name':products,'Price':prices}) 
+#df.to_csv('stocks.csv', index=False, encoding='utf-8')
 
     
-main()
 
